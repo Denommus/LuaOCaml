7f428f00bb0a275491488f6b3d172f0a462e1551
Yuri Albuquerque <yuridenommus@gmail.com>
2020-08-30 18:53:23 -0300

    Reading the file to parse from the command line.

--- executable/luaOCaml.ml
+++ executable/luaOCaml.ml
@@ -1,5 +1,14 @@
 open Lua.SedlexMenhir
 
-let () = create_lexbuf @@ Sedlexing.Utf8.from_string "foo['bar'] = function(a, b) local c={d = 10}; return a*b+c.d end"
-         |> sedlex_with_menhir Lua.LuaLexer.lexer Lua.LuaParser.chunk
-         |> Lua.Ast.show_block |> print_endline
+let parse_file file =
+  let channel = open_in file in
+  let result = Sedlexing.Utf8.from_channel channel
+               |> create_lexbuf ~file:file
+               |> sedlex_with_menhir Lua.LuaLexer.lexer Lua.LuaParser.chunk in
+  close_in channel;
+  result
+
+let () = let file_name = Array.get Sys.argv @@ Array.length Sys.argv - 1 in
+         parse_file file_name
+         |> Lua.Ast.show_block
+         |> print_endline
---
+++ executable/test.lua
@@ -0,0 +1,4 @@
+foo = {}
+foo['bar'] = function(a, b)
+   local c={d = 10}; return a*b+c.d
+end
